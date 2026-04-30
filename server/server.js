d9c8bd92bc8580f1766cd4a3a8392b56a9ffbbd6
Akshay Sheelam <sheelamakshay042@gmail.com>
2026-04-30 21:40:11 +0530

    connect frontend to railway backend

--- server/server.js
+++ server/server.js
@@ -6,22 +6,30 @@ require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
 const testRoutes = require("./routes/testRoutes");
-const projectRoutes = require("./routes/projectRoutes"); // 👈 ADD
-const taskRoutes = require("./routes/taskRoutes");       // 👈 ADD
+const projectRoutes = require("./routes/projectRoutes");
+const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://team-task-manager-wine-one.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
 
+app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/test", testRoutes);
-app.use("/api/projects", projectRoutes); // 👈 ADD
-app.use("/api/tasks", taskRoutes);  
-app.use("/api/users", userRoutes);     // 👈 ADD
+app.use("/api/projects", projectRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/users", userRoutes);
 
 // Test Route
 app.get("/", (req, res) => {
