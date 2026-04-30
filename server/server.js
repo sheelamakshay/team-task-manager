1f7371d0fe7a9ea1c1473103dff692d6baab7b8c
Akshay Sheelam <sheelamakshay042@gmail.com>
2026-04-30 20:43:20 +0530

    fix railway port binding

--- server/server.js
+++ server/server.js
@@ -34,8 +34,8 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.log(err));
 
 // PORT
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
